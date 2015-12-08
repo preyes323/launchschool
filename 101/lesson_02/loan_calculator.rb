463069baf2bb18e67c20e8ef07be35297a5cf652
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-12-08 18:12:24 +0800

    Complete calculator app

--- 101/lesson_02/loan_calculator.rb
+++ 101/lesson_02/loan_calculator.rb
@@ -7,6 +7,7 @@
 # -----------------------------------
 
 require 'colorize'
+require 'io/console'
 
 def msg(msg, color: :default, new_line: true)
   if new_line
@@ -24,7 +25,10 @@ def loan_amount_input
   loop do
     msg('Input loan amount: ', new_line: false)
     amount = gets.chomp
-    return Float(amount) if numeric?(amount)
+    if numeric?(amount)
+      return Float(amount) if amount.length <= 15
+      msg('Can not compute for loan amount with digits > 15', color: :red)
+    end
   end
 end
 
@@ -32,7 +36,7 @@ def monthly_percentage_rate_input
   loop do
     msg('Input annual percentage rate: ', new_line: false)
     apr = gets.chomp
-    return (Float(apr)/100/12).round(2) if numeric?(apr)
+    return (Float(apr) / 100 / 12).round(4) if numeric?(apr)
   end
 end
 
@@ -48,9 +52,79 @@ def add_padding(text, total_padding)
   text + ' ' * (total_padding - text.length)
 end
 
+def compute_monthly_payment(loan_params)
+  amt = loan_params[:amount]
+  rate = loan_params[:rate]
+  n = loan_params[:duration]
+  (amt * (rate * (1 + rate)**n) / ((1 + rate)**n - 1)).round(2)
+end
+
+def compute_monthly_balance(month, loan_params)
+  amt = loan_params[:amount]
+  rate = loan_params[:rate]
+  n = loan_params[:duration]
+  (amt * ((1 + rate)**n - (1 + rate)**month) / ((1 + rate)**n - 1)).round(2)
+end
+
+def compute_payments(loan_params)
+  balance, principal, interest = [[], [], []]
+  amount = loan_params[:amount]
+  duration = loan_params[:duration]
+  rate = loan_params[:rate]
+  monthly_payment = compute_monthly_payment(loan_params)
+  duration.times do |month|
+    interest << ((balance[-1] || amount) * rate).round(2)
+    balance << compute_monthly_balance(month + 1, loan_params)
+    principal << (monthly_payment - interest[-1]).round(2)
+  end
+  { monthly_payment: monthly_payment, principal: principal,
+    interest: interest, balance: balance }
+end
+
+def narrow_screen(payments)
+  msg(' MONTHLY PAYMENT SCHEDULE '.center(40, '-'), color: :blue)
+  payments[:balance].each_with_index do |_, month|
+    msg(" Month #{month + 1} ".center(40, '.'), color: :blue)
+    msg(add_padding('Payment:', 15), new_line: false)
+    msg("#{payments[:monthly_payment]}")
+    msg(add_padding('Principal:', 15), new_line: false)
+    msg("#{payments[:principal][month]}")
+    msg(add_padding('Interest:', 15), new_line: false)
+    msg("#{payments[:interest][month]}")
+    msg(add_padding('Balance:', 15), new_line: false)
+    msg("#{payments[:balance][month]}")
+  end
+end
+
+def wide_screen(payments)
+  msg(' MONTHLY PAYMENT SCHEDULE '.center(80, '-'), color: :blue)
+  msg("|#{'Month'.center(10, ' ')}|", new_line: false)
+  msg("#{'Payment'.center(16, ' ')}|", new_line: false)
+  msg("#{'Principal'.center(16, ' ')}|", new_line: false)
+  msg("#{'Interest'.center(16, ' ')}|", new_line: false)
+  msg("#{'Balance'.center(16, ' ')}|")
+  payments[:balance].each_with_index do |_, month|
+    msg("|#{(month + 1).to_s.center(10, ' ')}|", new_line: false)
+    msg("#{payments[:monthly_payment].to_s.center(16, ' ')}|", new_line: false)
+    msg("#{payments[:principal][month].to_s.center(16, ' ')}|", new_line: false)
+    msg("#{payments[:interest][month].to_s.center(16, ' ')}|", new_line: false)
+    msg("#{payments[:balance][month].to_s.center(16, ' ')}|")
+  end
+  msg('-' * 80, color: :blue)
+end
+
+def display_schedule(payments)
+  _, cols = $stdin.winsize
+  cols >= 80 ? wide_screen(payments) : narrow_screen(payments)
+end
+
 begin
   system 'clear'
-  msg('LOAN CALCULATOR APP', color: :blue)  
+  msg('The result of the app is best viewed ', color: :green, new_line: false)
+  msg('with a screen width of 80.', color: :green)
+  sleep(3)
+  system 'clear'
+  msg('LOAN CALCULATOR APP', color: :blue)
   msg('---')
   msg('This app produces your monthly payment schedule given the following:')
   msg('Loan Amount -  ex: $ 10,000.00)', color: :green)
@@ -66,18 +140,9 @@ begin
   msg("Loan Amount:       $#{loan_amount}")
   msg("Monthly % Rate:    #{monthly_percentage_rate}")
   msg("Duration (months): #{loan_duration_months}")
-  # monthly_payment = compute_payment(loan_amount,
-  #                                   monthly_percentage_rate,
-  #                                   loan_duration_months)  
-  msg(' MONTHLY PAYMENT SCHEDULE '.center(25, '-'), color: :blue)
-  loan_duration_months.times do |month|
-    msg(" Month #{month + 1} ".center(25, '.'))
-    msg(add_padding('Payment:', 12))
-    msg(add_padding('Balance:', 12))
-    msg(add_padding('Principal:', 12))
-    msg(add_padding('Interest:', 12))    
-  end
+  payments = compute_payments(amount:   loan_amount,
+                              rate:     monthly_percentage_rate,
+                              duration: loan_duration_months)
+  display_schedule(payments)
 end
-
-
 
