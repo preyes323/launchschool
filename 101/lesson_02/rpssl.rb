20749ea2a4cf414f2727e657563fced9a1cc7719
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-12-10 10:03:04 +0800

    Complete rpssl game

--- 101/lesson_02/rpssl.rb
+++ 101/lesson_02/rpssl.rb
@@ -14,6 +14,7 @@
 # --------------------------------------
 
 require 'colorize'
+require 'pry'
 
 CHOICES = { r: '[r]ock', p: '[p]aper', s: '[s]cissors',
             l: '[l]izard', k: 'spoc[k]' }
@@ -48,10 +49,12 @@ def print_game_header(name, scores)
   msg(header)
 end
 
-def print_player_area(player_move)
+def print_moves(player_move, comp_move)
   move = CHOICES[player_move] || ''
+  comp_move = CHOICES[comp_move] || ''
   msg(" #{' ' * 18}|#{' ' * 18} ")
-  msg(" #{move.center(18, ' ')}|")
+  msg(" #{move.center(18, ' ')}|", new_line: false)
+  msg(" #{comp_move.center(18, ' ')}")
   msg(" #{' ' * 18}|#{' ' * 18} ")
 end
 
@@ -65,23 +68,142 @@ def player_move_input
   end
 end
 
-def display_game_board(name, scores, player_move: nil, computer_move: nil)
-  print_game_header(name, scores)
-  print_player_area(player_move)
-  #print_computer_area(computer_move)
+def computer_move_randomizer
+  moves = []
+  9.upto(50) do
+    moves << CHOICES.keys.sample
+  end
+  moves
+end
+
+def display_game_board(name, scores, old_scores: nil,
+                                     player_move: nil,
+                                     computer_move: [nil])
+  computer_move.each_with_index do |comp_move, idx|
+    system 'clear'
+    if idx == computer_move.length - 1
+      print_game_header(name, scores)
+    else
+      print_game_header(name, old_scores)
+    end
+    print_moves(player_move, comp_move)
+    sleep(0.05)
+  end
   msg('')
 end
 
+def update_scores!(scores, winner)
+  if winner == :player
+    scores[0] += 1
+  elsif winner == :computer
+    scores[1] += 1
+  end
+end
+
+def game_winner(player_move, computer_move)
+  return :tie if player_move == computer_move
+  case player_move
+  when :s
+    if computer_move == :p || computer_move == :l
+      :player
+    else
+      :computer
+    end
+  when :p
+    if computer_move == :r || computer_move == :k
+      :player
+    else
+      :computer
+    end
+  when :r
+    if computer_move == :l || computer_move == :s
+      :player
+    else
+      :computer
+    end
+  when :l
+    if computer_move == :p || computer_move == :k
+      :player
+    else
+      :computer
+    end
+  when :k
+    if computer_move == :s || computer_move == :r
+      :player
+    else
+      :computer
+    end
+  end
+end
+
+def display_winning_message(move1, move2)
+  message = case move1
+            when :s
+              case move2
+              when :p then 'Scissors cut Paper!'
+              when :l then 'Scissors decapitates Lizard!'
+              when :r then 'Rock crushes Scissors!'
+              when :k then 'Spock smashes Scissors!'
+              end
+            when :p
+              case move2
+              when :r then 'Paper covers Rock!'
+              when :k then 'Paper disproves Spock!'
+              when :s then 'Scissors cut Paper!'
+              when :l then 'Lizard eats Paper!'
+              end
+            when :r
+              case move2
+              when :s then 'Rock crushes Scissors!'
+              when :l then 'Rock crushes Lizard!'
+              when :p then 'Paper covers Rock!'
+              when :k then 'Spock vaporizes Rock!'
+              end
+            when :l
+              case move2
+              when :k then 'Lizard poisons Spock!'
+              when :p then 'Lizard eats Paper!'
+              when :s then 'Scissors decapitates Lizard!'
+              when :r then 'Rock crushes Lizard!'
+              end
+            when :k
+              case move2
+              when :s then 'Spock smashes Scissors!'
+              when :r then 'Spock vaporizes Rock!'
+              when :l then 'Lizard poisons Spock!'
+              when :p then 'Paper disproves Spock!'
+              end
+            end
+  message ||= 'We think alike!!!'
+  msg(message.center(39, ' '), color: :light_blue)
+  msg(' ')
+end
+
+def play_again?
+  msg('Play another round? (y/n)', color: :light_blue)
+  msg('=> ', color: :blue, new_line: false)
+  gets.chomp == 'y'
+end
+
+system 'clear'
+display_welcome_message
+msg('-' * 10, color: :light_blue)
+name = player_name
+scores = [0, 0]
+sleep(2)
+system 'clear'
+
 loop do
-  system 'clear'
-  display_welcome_message
-  msg('-' * 10, color: :light_blue)
-  name = player_name
-  scores = [0, 0]
-  sleep(3)
-  system 'clear'
   display_game_board(name, scores.map(&:to_s))
   move = player_move_input
-  display_game_board(name, scores.map(&:to_s), player_move: move)
-  break
+  comp_moves = computer_move_randomizer
+  winner = game_winner(move, comp_moves[-1])
+  old_scores = scores.dup
+  update_scores!(scores, winner)
+  display_game_board(name, scores.map(&:to_s),
+                     old_scores: old_scores.map(&:to_s),
+                     player_move: move,
+                     computer_move: comp_moves)
+  display_winning_message(move, comp_moves[-1])
+  break unless play_again?
 end
