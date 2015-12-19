5b839fe30f9d28f86864b264638e34905c6ad4fd
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-12-20 05:31:46 +0800

    Complete octal problem

--- coding-challenges/octal/octal.rb
+++ coding-challenges/octal/octal.rb
@@ -1,9 +1,11 @@
 class Octal
   def initialize(octal)
-    @octal = octal =~ /[^0-7]/ ? [0] : octal.chars.map(&:to_i)
+    @octal = octal =~ /[^0-7]/ ? [0] : octal.chars.map(&:to_i).reverse
   end
 
   def to_decimal
-
+    @octal
+      .each_with_index
+      .inject(0) { |decimal, (value, idx)| decimal + value * 8 ** idx }
   end
 end
