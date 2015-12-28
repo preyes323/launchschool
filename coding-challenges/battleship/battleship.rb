01ac446132d9814417eb8a0a4464c6190640dc30
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-12-28 14:21:12 +0800

    Complete test suite for battleship board; TODO: pass board creation test suite

--- coding-challenges/battleship/battleship.rb
+++ coding-challenges/battleship/battleship.rb
@@ -48,6 +48,98 @@ class Computer < Player
 end
 
 class Ship
-  SHIPS = %w(destroyer, cruiser, battleship)
+  attr_accessor :hitpoints, :type
 
+  def initialize(type)
+    self.type = type
+    self.hitpoints = initialize_hitpoints(type)
+  end
+
+  def to_s
+    "#{type}: #{hitpoints}"
+  end
+
+  private
+
+  def initialize_hitpoints(type)
+    case type
+    when 'destroyer' then 1
+    when 'cruiser' then 2
+    when 'battleship' then 3
+    else 0
+    end
+  end
+end
+
+class Board
+  attr_accessor :board, :location_marks
+
+  def initialize(*sides)
+    @board = []
+    binding.pry
+    if sides.length == 2
+      location_marks = Array.new(sides[0]) { Array.new(sides[1]) }
+      build_board(sides[0], sides[1])
+    else
+      location_marks = Array.new(sides[0]) { Array.new(sides[0]) }
+      build_board(sides[0], sides[1])
+    end
+  end
+
+  def display_board
+
+  end
+
+  def to_s
+
+  end
+
+  private
+
+  def build_board(rows, cols)
+    (rows + 1).times do |row|
+      if row == 0 then
+        self.board << draw_top_row(cols)
+      else
+        self.board << draw_top_bottom_cell(cols + 1)
+        self.board << draw_middle_cell(row, cols + 1)
+      end
+    end
+    self.board << draw_top_bottom_cell(cols + 1)
+  end
+
+  def draw_marker(row, col)
+    "#{locations_marks[row][col].to_s.center(3, ' ')}"
+  end
+
+  def draw_top_row(cols)
+    result = []
+    cols.times do |col|
+      result << col == 0 ? "#{    (col + 1)}" : "#{   (col + 1)}"
+    end
+  end
+
+  def draw_top_bottom_cell(cols)
+    result = []
+    cols.times do |col|
+      result << case col
+                when 0 then '  '
+                when cols - 1 then "+---+\n"
+                else '+---'
+                end
+    end
+    result.join
+  end
+
+  def draw_middle_cell(row, cols)
+    result = []
+    cols.times do |col|
+      result << case col
+                when 0 then "#{row} "
+                when cols - 1 then "|#{draw_marker(row, col)}|\n"
+                else "|#{draw_marker(row, col)}"
+                end
+    end
+    result.join
+  end
 end
--- coding-challenges/battleship/ts_battleship.rb
+++ coding-challenges/battleship/ts_battleship.rb
@@ -36,21 +36,62 @@ end
 
 describe Ship do
   describe 'when creating a new ship' do
-    it 'must return true if ship type set is valid' do
-
-    end
-
-    it "must return 'invalid ship type' if ship is not in SHIPS" do
-    end
-
     it 'must have 1 hitpoint if destroyer' do
-      @ship.new('destroyer').hitpoints.must_equal 1
+      Ship.new('destroyer').hitpoints.must_equal 1
     end
 
     it 'must have 2 hitpoints if cruiser' do
+      Ship.new('cruiser').hitpoints.must_equal 2
     end
 
     it 'must have 3 hitpoints if battleship' do
+      Ship.new('battleship').hitpoints.must_equal 3
+    end
+  end
+
+  describe 'when printing out display of Ship details (to_s)' do
+    it 'must have valid shipname and hitpoint amount' do
+      Ship.new('cruiser').to_s
+                         .must_match /cruiser|destroyer|battleship|:\s[0-3]/
+    end
+  end
+end
+
+describe Board do
+  describe 'when create a new board' do
+    it 'must successfully create an empty 5x5 board' do
+      board_5x5 = %(    1   2   3   4   5
+  +---+---+---+---+---+
+1 |   |   |   |   |   |
+  +---+---+---+---+---+
+2 |   |   |   |   |   |
+  +---+---+---+---+---+
+3 |   |   |   |   |   |
+  +---+---+---+---+---+
+4 |   |   |   |   |   |
+  +---+---+---+---+---+
+5 |   |   |   |   |   |
+  +---+---+---+---+---+
+).chomp
+
+      Board.new(5).board.must_equal board_5x5
+    end
+
+    it 'must successfully create an empty 5x7 board' do
+      board_5x7 = %(    1   2   3   4   5   6   7
+  +---+---+---+---+---+---+---+
+1 |   |   |   |   |   |   |   |
+  +---+---+---+---+---+---+---+
+2 |   |   |   |   |   |   |   |
+  +---+---+---+---+---+---+---+
+3 |   |   |   |   |   |   |   |
+  +---+---+---+---+---+---+---+
+4 |   |   |   |   |   |   |   |
+  +---+---+---+---+---+---+---+
+5 |   |   |   |   |   |   |   |
+  +---+---+---+---+---+---+---+
+).chomp
+      Board.new(5, 7).board.must_equal board_5x7
     end
   end
 end
