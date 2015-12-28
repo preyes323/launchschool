558ee6f595fd20207492371fd3dbdb8bf88866ae
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-12-28 21:58:41 +0800

    Complete creating board for battleship

--- coding-challenges/battleship/battleship.rb
+++ coding-challenges/battleship/battleship.rb
@@ -75,14 +75,13 @@ class Board
   attr_accessor :board, :location_marks
 
   def initialize(*sides)
-    @board = []
-    binding.pry
+    @board = ''
     if sides.length == 2
-      location_marks = Array.new(sides[0]) { Array.new(sides[1]) }
+      self.location_marks = Array.new(sides[0]) { Array.new(sides[1]) }
       build_board(sides[0], sides[1])
     else
-      location_marks = Array.new(sides[0]) { Array.new(sides[0]) }
-      build_board(sides[0], sides[1])
+      self.location_marks = Array.new(sides[0]) { Array.new(sides[0]) }
+      build_board(sides[0], sides[0])
     end
   end
 
@@ -97,49 +96,53 @@ class Board
   private
 
   def build_board(rows, cols)
-    (rows + 1).times do |row|
-      if row == 0 then
-        self.board << draw_top_row(cols)
-      else
-        self.board << draw_top_bottom_cell(cols + 1)
-        self.board << draw_middle_cell(row, cols + 1)
-      end
+    rows.times do |row|
+      self.board << draw_top_row(cols) if row == 0
+      self.board << draw_top_bottom_cell(cols)
+      self.board << draw_middle_cell(row, cols)
     end
-    self.board << draw_top_bottom_cell(cols + 1)
+    self.board << draw_top_bottom_cell(cols).chomp
   end
 
   def draw_marker(row, col)
-    "#{locations_marks[row][col].to_s.center(3, ' ')}"
+    "#{location_marks[row][col].to_s.center(3, ' ')}"
   end
 
   def draw_top_row(cols)
-    result = []
+    result = ''
+
     cols.times do |col|
-      result << col == 0 ? "#{    (col + 1)}" : "#{   (col + 1)}"
+      result << (col == 0 ? "    #{(col + 1)}" : "   #{(col + 1)}")
     end
+
+    result << "\n"
   end
 
   def draw_top_bottom_cell(cols)
-    result = []
+    result = ''
+
     cols.times do |col|
       result << case col
-                when 0 then '  '
+                when 0 then '  +---'
                 when cols - 1 then "+---+\n"
                 else '+---'
                 end
     end
-    result.join
+
+    result
   end
 
   def draw_middle_cell(row, cols)
-    result = []
+    result = ''
+
     cols.times do |col|
       result << case col
-                when 0 then "#{row} "
-                when cols - 1 then "|#{draw_marker(row, col)}|\n"
+                when 0 then "#{row + 1} |#{draw_marker(row, col)}"
+                when cols - 1 then "|#{draw_marker(row - 1, col - 1)}|\n"
                 else "|#{draw_marker(row, col)}"
                 end
     end
-    result.join
+
+    result
   end
 end
