98b3bd2395fc4f9a5c8ce1b91efa11186a5a31b1
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-02-20 11:41:51 +0800

    Working on search page for bookviewer app

--- 170/lesson_04/book_viewer/book_viewer.rb
+++ 170/lesson_04/book_viewer/book_viewer.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
@@ -16,7 +17,39 @@ get "/chapters/:number" do
   ch_name = @contents[number - 1]
 
   @title = "Chapter #{number}: #{ch_name}"
-  @chapter = File.read("data/chp#{number}.txt")
+  @chapter = load_chapter("data/chp#{number}.txt")
+
 
   erb :chapter
+end
+
+get "/search" do
+  @phrase = params[:query]
+  if @phrase
+    @chapters = @contents.each_with_index.select do |_, idx|
+      phrase_exists?(load_chapter("data/chp#{idx + 1}.txt"), @phrase)
+    end
+  end
+
+  erb :search
+end
+
+not_found do
+  redirect '/'
+end
+
+helpers do
+  def in_paragraphs(content)
+    content.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+
+  def phrase_exists?(chapter, phrase)
+    chapter =~ /#{phrase}/
+  end
+
+  def load_chapter(chapter)
+    File.read(chapter)
+  end
 end
