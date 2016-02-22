c95f6446515a17ef97164d44b23ee93881915ae3
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-02-23 04:28:37 +0800

    Working on users-interests challenge

--- 170/lesson_04/book_viewer/book_viewer.rb
+++ 170/lesson_04/book_viewer/book_viewer.rb
@@ -26,9 +26,9 @@ end
 get "/search" do
   @phrase = params[:query]
   if @phrase
-    @chapters = @contents.each_with_index.select do |_, idx|
-      phrase_exists?(load_chapter("data/chp#{idx + 1}.txt"), @phrase)
-    end
+    @chapters = @contents.each_with_index.each_with_object({}) do |(_, idx), result|
+      result[idx + 1] = phrase_indexes(load_chapter("data/chp#{idx + 1}.txt"), @phrase)
+    end.delete_if { |_, value| value.empty? }
   end
 
   erb :search
@@ -40,13 +40,20 @@ end
 
 helpers do
   def in_paragraphs(content)
-    content.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+    paragraphs(content).each_with_index.map do |paragraph, number|
+      "<p id=paragraph#{number}>#{paragraph}</p>"
     end.join
   end
 
-  def phrase_exists?(chapter, phrase)
-    chapter =~ /#{phrase}/
+  def paragraphs(content)
+    content.split("\n\n")
+  end
+
+  def phrase_indexes(chapter, phrase)
+    paragraphs(chapter).each_with_index
+      .each_with_object({}) do |(paragraph, index), results|
+      results[index] = paragraph if paragraph =~ /#{phrase}/
+    end
   end
 
   def load_chapter(chapter)
---
+++ 170/lesson_04/users-interests/router.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+require 'yaml'
+require 'pry'
+
+before do
+  @users = YAML.load_file('public/users.yml')
+  @interests = count_interests(@users)
+end
+
+get "/" do
+  binding.pry
+  erb :home
+end
+
+helpers do
+  def count_interests(users)
+    users.values
+  end
+end
