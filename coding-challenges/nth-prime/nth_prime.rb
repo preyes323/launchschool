0ed167f6dee164e461b77819f69729653fe782b3
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-02 01:51:19 +0800

    Complete nth-prime challenge

---
+++ coding-challenges/nth-prime/nth_prime.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Prime
+  def self.nth(count)
+    raise ArgumentError if count == 0
+    primes.take(count).max[0]
+  end
+
+
+  def self.primes
+    (2..Float::INFINITY).lazy.with_object([]).select do |num, result|
+      if result.empty? || result.all? { |prime_num| num % prime_num != 0 }
+        result << num
+      else
+        false
+      end
+    end
+  end
+end
