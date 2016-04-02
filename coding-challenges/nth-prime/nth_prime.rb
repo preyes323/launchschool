01308cc96c94c00c827908019ec341d8bfe4d344
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-02 10:15:36 +0800

    Refactor nth_primes to optimize run time

--- coding-challenges/nth-prime/nth_prime.rb
+++ coding-challenges/nth-prime/nth_prime.rb
@@ -1,12 +1,14 @@
 class Prime
   def self.nth(count)
     raise ArgumentError if count == 0
-    primes.take(count).max[0]
+    primes.take(count).max.first
   end
 
   def self.primes
     (2..Float::INFINITY).lazy.with_object([]).select do |num, result|
-      if result.empty? || result.all? { |prime_num| num % prime_num != 0 }
+      factors = result.take_while { |factor| factor <= Math.sqrt(num) }
+
+      if result.empty? || factors.all? { |prime_num| num % prime_num != 0 }
         result << num
       else
         false
