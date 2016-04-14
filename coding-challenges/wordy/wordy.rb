d33344e0cea29fc8ed809b7f55c6e40ea88375ff
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-14 16:12:48 +0800

    Complete wordy challenge

---
+++ coding-challenges/wordy/wordy.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class WordProblem
+  OPERANDS = { 'plus' => '+', 'minus' => '-',
+                'divided' => '/', 'multiplied' => '*' }
+
+  attr_reader :operands, :numbers
+
+  def initialize question
+    @numbers = question.scan(/-?\d+/).map(&:to_i)
+    @operands = question.scan(/plus|minus|divided|multiplied/)
+
+    raise ArgumentError, "Question not recognized" if @numbers.empty? || @operands.empty?
+  end
+
+  def answer
+    result = @numbers.first
+
+    @operands.each_with_index do |operand, idx|
+      result = result.send(OPERANDS[operand], @numbers[idx + 1])
+    end
+
+    result
+  end
+end
