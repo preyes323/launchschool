c7bc4632f693d398b45d0d1b1b49c993055db3db
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-24 03:07:54 +0800

    Complete refactor of anagrams challenge; not using .sort method

--- coding-challenges/anagrams/anagram.rb
+++ coding-challenges/anagrams/anagram.rb
@@ -1,23 +1,17 @@
 class Anagram
   def initialize word
     @word = word
-    @word_letters = to_letters word
+    @word_letters = to_letters_hash word
   end
 
   def match candidates
-
-    candidates.each_with_object([]) do |candidate, anagrams|
+    candidates.select do |candidate|
       next if @word.downcase == candidate.downcase
-      candidate_letters = to_letters candidate
-      anagrams << candidate if all_letters_match? candidate_letters
+      @word_letters == to_letters_hash(candidate)
     end
   end
 
-  def all_letters_match? letters
-    @word_letters.keys.all? { |letter| letters[letter] == @word_letters[letter] }
-  end
-
-  def to_letters word
+  def to_letters_hash word
     word.downcase.chars.each_with_object({}) do |letter, collection|
       collection[letter] ||= 0
       collection[letter] += 1
