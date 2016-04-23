7a966db9a27efcd4b7246f4d324e52273e94d2bf
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-24 03:11:20 +0800

    add the original anagram coding challenge solution

--- coding-challenges/anagrams/anagram.rb
+++ coding-challenges/anagrams/anagram.rb
@@ -1,3 +1,22 @@
+## Original code challenge version
+class Anagram
+  def initialize word
+    @word = word
+  end
+
+  def match candidates
+    candidates.each_with_object([]) do |candidate, anagrams|
+      next if @word.downcase == candidate.downcase
+      anagrams << candidate if all_letters_match? candidate.downcase.chars
+    end
+  end
+
+  def all_letters_match? letters
+    @word.downcase.chars.sort == letters.sort
+  end
+end
+
+## Live challenge session version
 class Anagram
   def initialize word
     @word = word
