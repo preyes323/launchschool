becf29fb2c67c2b0c62160a158d67e509edaf383
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-04-24 02:41:42 +0800

    modifying anagrams challenge

--- coding-challenges/anagrams/anagram.rb
+++ coding-challenges/anagrams/anagram.rb
@@ -1,16 +1,26 @@
 class Anagram
   def initialize word
     @word = word
+    @word_letters = to_letters word
   end
 
   def match candidates
+
     candidates.each_with_object([]) do |candidate, anagrams|
       next if @word.downcase == candidate.downcase
-      anagrams << candidate if all_letters_match? candidate.downcase.chars
+      candidate_letters = to_letters candidate
+      anagrams << candidate if all_letters_match? candidate_letters
     end
   end
 
   def all_letters_match? letters
-    @word.downcase.chars.sort == letters.sort
+    @word_letters.keys.all? { |letter| letters[letter] == @word_letters[letter] }
+  end
+
+  def to_letters word
+    word.downcase.chars.each_with_object({}) do |letter, collection|
+      collection[letter] ||= 0
+      collection[letter] += 1
+    end
   end
 end
