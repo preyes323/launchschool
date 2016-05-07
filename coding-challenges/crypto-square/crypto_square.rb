b450dfc248de4fa3c2509f96ae816085f7ed7dbd
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-05-08 03:24:02 +0800

    Minor optimization on crypto challenge

--- coding-challenges/crypto-square/crypto_square.rb
+++ coding-challenges/crypto-square/crypto_square.rb
@@ -2,19 +2,16 @@ class Crypto
   DUMMY_CHAR = '!'
 
   def initialize(plaintext)
-    @plaintext = plaintext
-  end
-
-  def normalize_plaintext
-    @plaintext.gsub(/[^a-z0-9]/i, '').downcase
+    @normalize_plaintext = plaintext.gsub(/[^a-z0-9]/i, '').downcase
+    @size = size
   end
 
   def size
-    ((normalize_plaintext.size)**(0.5)).ceil
+    ((@normalize_plaintext.size)**(0.5)).ceil
   end
 
   def plaintext_segments
-    segment_text normalize_plaintext, size
+    segment_text @normalize_plaintext, size
   end
 
   def ciphertext
