1f94b28f64ee3f80fe7f06757a85f75e5030375b
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-06-03 15:59:26 +0800

    Complete house challenge; pushed other 201 exercises

---
+++ coding-challenges/house/house.rb
@@ -0,0 +1,39 @@
+class House
+  def self.recite
+    lines = House.new.send(:pieces)
+    @result = nil
+
+    house = lines.reverse.each_with_object([]) do |line, stanzas|
+      if @result
+        @result.gsub!('This is ','').gsub!('.', '')
+        @result = "This is " + [line.first, [line.last,  @result].join(' ')].join("\n")
+      else
+        @result ||= ['This is', line.first].join(' ')
+      end
+
+      @result << '.'
+      stanzas << @result.dup
+    end
+
+    house.join("\n\n") << "\n"
+  end
+
+  private
+
+  def pieces
+    [
+      ['the horse and the hound and the horn', 'that belonged to'],
+      ['the farmer sowing his corn', 'that kept'],
+      ['the rooster that crowed in the morn', 'that woke'],
+      ['the priest all shaven and shorn', 'that married'],
+      ['the man all tattered and torn', 'that kissed'],
+      ['the maiden all forlorn', 'that milked'],
+      ['the cow with the crumpled horn', 'that tossed'],
+      ['the dog', 'that worried'],
+      ['the cat', 'that killed'],
+      ['the rat', 'that ate'],
+      ['the malt', 'that lay in'],
+      ['the house that Jack built']
+    ]
+  end
+end
