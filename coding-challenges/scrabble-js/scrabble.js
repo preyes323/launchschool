4233df311e53d6b3b3ccb078666e6fb46665f24b
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-08-05 21:03:07 +0800

    more cleanup

--- coding-challenges/scrabble-js/scrabble.js
+++ coding-challenges/scrabble-js/scrabble.js
@@ -1,6 +1,6 @@
 (function() {
   var scrabble = function(word) {
-    var u = {
+    return {
       score: (function() {
         var CHARACTER_SCORES = { "A": 1, "E": 1, "I": 1, "O": 1, "U": 1, "L": 1, "N": 1, "R": 1,
                                  "S": 1, "T": 1, "D": 2, "G": 2, "B": 3, "C": 3, "M": 3, "P": 3,
@@ -22,8 +22,6 @@
         };
       })()
     };
-
-    return u;
   };
 
   scrabble.score = function(word) {
