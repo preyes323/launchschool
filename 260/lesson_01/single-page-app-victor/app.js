feb4a55028b8845232fbdca60808179d42b7ce59
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-10-18 00:38:43 +0800

    TODO: add public/

---
+++ 260/lesson_01/single-page-app-victor/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const events = require('./routes/events');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/events', events);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
+
+module.exports = app;
---
+++ 260/lesson_01/single-page-app-victor/routes/events.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
