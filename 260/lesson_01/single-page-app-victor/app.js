ef1a306e01640b90869427a188de70277f25803c
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-10-19 00:06:13 +0800

    Begins work on building own single page events app

--- 260/lesson_01/single-page-app-victor/app.js
+++ 260/lesson_01/single-page-app-victor/app.js
@@ -9,9 +9,6 @@ const events = require('./routes/events');
 const app = express();
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/events', events);
 
--- 260/lesson_01/single-page-app-victor/routes/events.js
+++ 260/lesson_01/single-page-app-victor/routes/events.js
@@ -2,3 +2,5 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
