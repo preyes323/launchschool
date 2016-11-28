24f7e4eb6b5bfea7e11f70ad413f87ca178f8e9d
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2016-11-28 09:28:37 +0800

    WIP: begins work on 260 lesson 1 TODO MVC

---
+++ 260/lesson_01/todoMVC/README.md
@@ -0,0 +1,15 @@
+### General Requirements
+
+Build a todo app using the following [HTML and CSS](http://d3905n0khyu9wc.cloudfront.net/todo_app/todo_app.zip) as starting point. The todo app will use the MVC framework that was built earlier in the course.
+
+Here's a link to the course page for the [complete requirements document](https://launchschool.com/lessons/fae4fa27/assignments/4953c85b).
+
+### Specific Requirements
+
+* Typing in a todo and submitting the form creates a new todo item
+* Clicking the check box for a todo toggles its completed state
+* Clicking the text for the todo toggles editing for that todo
+* Clicking outside of the text input for a currently editing todo saves the edit
+* Clicking clear complete will find all completed todos and remove them
+
+
---
+++ 260/lesson_01/todoMVC/javascripts/templates.js
@@ -0,0 +1,18 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['todo'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "<a href=\"#\" class=\"toggle\"></a>\n"
+    + container.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"name","hash":{},"data":data}) : helper)));
+},"useData":true});
+templates['todoEdit'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<li id=\"edit-todo\" data-id=\""
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\">\n  <input type=\"text\" value=\""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\" />\n</li>";
+},"useData":true});
+})();
