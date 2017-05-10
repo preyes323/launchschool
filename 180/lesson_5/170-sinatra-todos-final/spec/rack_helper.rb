419a7a37d010a7ca1c4d2b1d7c6d3486953fea6b
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2017-05-10 16:57:05 +0800

    WIP: expense tracker

---
+++ 180/lesson_5/170-sinatra-todos-final/spec/rack_helper.rb
@@ -0,0 +1,11 @@
+require 'rack/test'
+require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
